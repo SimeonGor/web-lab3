e9c474686211cccb6bcc85db78fbfeba8b2038bd
simeon.gor <simeon.gor@yandex.ru>
2025-05-04 19:23:46 +0300

    feat: add test

---
+++ src/test/java/com/simeon/lab3/services/CheckUtilTest.java
@@ -0,0 +1,13 @@
+package com.simeon.lab3.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CheckUtilTest {
+
+    @Test
+    void check() {
+        assertTrue(true);
+    }
+}
