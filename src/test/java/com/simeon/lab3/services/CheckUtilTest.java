56b7a81d325252fa1838e0efdf7c273f13fcc865
simeon.gor <simeon.gor@yandex.ru>
2025-05-05 00:18:31 +0300

    feat: update test

--- src/test/java/com/simeon/lab3/services/CheckUtilTest.java
+++ src/test/java/com/simeon/lab3/services/CheckUtilTest.java
@@ -1,13 +1,23 @@
 package com.simeon.lab3.services;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CheckUtilTest {
 
-    @Test
-    void check() {
-        assertTrue(true);
+    @ParameterizedTest
+    @ValueSource(strings = {"2", "3", "4", "5"})
+    void checkFirstQuarterBorderHit(String string_r) {
+        BigDecimal r = new BigDecimal(string_r);
+        BigDecimal x = r.divide(BigDecimal.valueOf(Math.sqrt(2)), RoundingMode.FLOOR);
+        BigDecimal y = x;
+
+        assertTrue(CheckUtil.check(x, y, r));
     }
 }
